ee7c4706ad987129da9ccd8f390484cf8b992bb7
g-g-gerin <124514292+g-g-gerin@users.noreply.github.com>
2024-04-06 12:59:22 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
+    const char* txt = "I Love UNN!";
+    std::cout << "faStr1: " << faStr1(txt) << std::endl;
+    std::cout << "faStr2: " << faStr2(txt) << std::endl;
+    std::cout << "faStr3: " << faStr3(txt) << std::endl;
     return 0;
 }
